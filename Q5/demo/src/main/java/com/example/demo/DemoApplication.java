df2cedf306d1fe6f240aa0f26c676b663e100f25
Piyush Mehta <me@piyushmehta.com>
2020-01-23 15:20:05 +0530

    Scheduler

--- Q5/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Q5/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableScheduling
 public class DemoApplication extends SpringBootServletInitializer{
 
 	@Override
