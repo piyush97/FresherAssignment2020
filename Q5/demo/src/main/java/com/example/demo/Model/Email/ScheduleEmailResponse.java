f160aa9b48db90d760af053ecce6ba9af8cc3f78
Piyush Mehta <me@piyushmehta.com>
2020-01-23 11:12:16 +0530

    Response

--- Q5/demo/src/main/java/com/example/demo/Model/Email/ScheduleEmailResponse.java
+++ Q5/demo/src/main/java/com/example/demo/Model/Email/ScheduleEmailResponse.java
@@ -1,4 +1,29 @@
 package com.example.demo.Model.Email;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Getter @Setter @NoArgsConstructor
 public class ScheduleEmailResponse {
+
+    private boolean success;
+    private String jobId;
+    private String jobGroup;
+    private String message;
+
+    public ScheduleEmailResponse(boolean sucess, String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+    public ScheduleEmailResponse(boolean success, String jobId, String jobGroup, String message) {
+        this.success = success;
+        this.jobId = jobId;
+        this.jobGroup = jobGroup;
+        this.message = message;
+    }
+
 }
