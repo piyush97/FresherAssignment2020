125f8c3f39f31ec866948a72cfaec5a01ff67e56
Piyush Mehta <me@piyushmehta.com>
2020-01-24 10:39:44 +0530

    Detail service

--- Q5/demo/src/main/java/com/example/demo/Service/MyUserDetailsService.java
+++ Q5/demo/src/main/java/com/example/demo/Service/MyUserDetailsService.java
@@ -1,16 +1,18 @@
 package com.example.demo.Service;
 
+import com.example.demo.Model.MyUserDetails;
 import com.example.demo.Model.User;
 import com.example.demo.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
-public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+public class MyUserDetailsService implements UserDetailsService {
 
     @Autowired
     UserRepository userRepository;
@@ -21,6 +23,6 @@ public class UserDetailsService implements org.springframework.security.core.use
 
         user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + emailId));
 
-        return user.map(User).get();
+        return user.map(MyUserDetails::new).get();
     }
 }
