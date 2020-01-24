945ae55ff1e9239d83101c3e90da07bdf9170c15
Piyush Mehta <me@piyushmehta.com>
2020-01-24 10:39:18 +0530

    UserRepo

--- Q5/demo/src/main/java/com/example/demo/Repositories/UserRepository.java
+++ Q5/demo/src/main/java/com/example/demo/Repositories/UserRepository.java
@@ -4,7 +4,11 @@ import com.example.demo.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 //    List<NewsLetter> findByTitleContaingOrContentContaining(String text, String textAgain);
+Optional<User> findByEmailId(String emailId);
+
 }
