38362ce61f40f2651289a5db1524e60aa934f898
Piyush Mehta <me@piyushmehta.com>
2020-01-24 10:39:08 +0530

    Userr model

--- Q5/demo/src/main/java/com/example/demo/Model/User.java
+++ Q5/demo/src/main/java/com/example/demo/Model/User.java
@@ -6,15 +6,66 @@ import lombok.NonNull;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.Set;
 
 @Entity
 @Getter @Setter @NoArgsConstructor
 public class User {
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private @NonNull int  userId;
     private @NonNull String emailId;
+    private @NonNull String userName;
+    private @NonNull String password;
+    private @NonNull String roles;
+    private boolean active;
 
     public User(int userId, String emailId) {
         this.userId = userId;
